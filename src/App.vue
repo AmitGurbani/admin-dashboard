383ed2a21d072dd4c4963db2763f9ce5744450f6
Amit Gurbani <amit1994.gurbani@gmail.com>
2021-11-04 20:43:11 +0530

    Updated email for login

--- src/App.vue
+++ src/App.vue
@@ -78,7 +78,7 @@ export default class App extends Vue {
 
   verifyUser() {
     if (
-      this.email === "admin@cloudworx.com" &&
+      this.email === "admin@cloudworx.ai" &&
       this.password === "securepassword"
     ) {
       this.email = "";
